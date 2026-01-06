309f5ce10c00da5442ec14a3fc3440ac0e580695
alfredomota90 <a.mota.perez@gmail.com>
2026-01-06 12:29:52 -0600

    add getbook and send request to api to create book

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,9 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { loader as booksLoader } from "./services/booksLoader";
 import Home from "./UI/Home";
-import Adminbooks from "./features/adminbooks/Adminbooks";
+import Adminbooks, {
+  action as createBookAction,
+} from "./features/adminbooks/Adminbooks";
 import Booklist from "./features/booklist/Booklist";
 import BookDetails from "./features/showbooks/BookDetails";
 import AppLayout from "./UI/AppLayout";
@@ -24,6 +26,7 @@ const router = createBrowserRouter([
       {
         path: "/adminBooks",
         element: <Adminbooks />,
+        action: createBookAction,
       },
       {
         path: "/bookList",
--- frontend/src/features/adminbooks/Adminbooks.jsx
+++ frontend/src/features/adminbooks/Adminbooks.jsx
@@ -1,18 +1,57 @@
+import { Form, redirect } from "react-router-dom";
+import { postBook } from "../../services/apibooks";
+
 function Adminbooks() {
   return (
-    <form>
+    <Form method="POST">
       <h2>Add new book</h2>
-      <input placeholder="Title" />
-      <br></br>
-      <input placeholder="Author" />
-      <br></br>
-      <input placeholder="Price" />
-      <br></br>
-      <input placeholder="Image URL" />
-      <br></br>
+      <div>
+        <label>Title</label>
+        <input type="text" name="title" />
+      </div>
+
+      <div>
+        <label>Author</label>
+        <input type="text" name="author" />
+      </div>
+
+      <div>
+        <label>Price</label>
+        <input
+          type="number"
+          name="price"
+          required="true"
+          min={0.01}
+          step={0.01}
+        />
+      </div>
+
+      <div>
+        <label>Image URL</label>
+        <input type="text" name="imageUrl" />
+      </div>
       <button>Add Book</button>
-    </form>
+    </Form>
   );
+}
+
+export async function action({ request }) {
+  const formData = await request.formData();
+  const data = Object.fromEntries(formData);
+  console.log(data);
+
+  const book = {
+    id: 0,
+    title: data.title,
+    author: data.author,
+    price: parseFloat(data.price),
+    imageUrl: data.imageUrl,
+  };
+  console.log(book);
+
+  await postBook(book);
+
+  return redirect(`/bookDetails/${book.id}`);
 }
 
 export default Adminbooks;
