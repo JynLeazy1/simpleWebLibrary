9e95f93c09468eeb08d582393c5a2229f7c3a003
alfredomota90 <a.mota.perez@gmail.com>
2026-03-13 13:11:51 -0600

    add login logic

--- frontend/src/UI/Login.jsx
+++ frontend/src/UI/Login.jsx
@@ -1,18 +1,37 @@
 import { useNavigate } from "react-router-dom";
+import { login } from "../services/login";
 
 function Login() {
   const navigate = useNavigate();
   function handleSubmit(e) {
     e.preventDefault();
+
+    const formData = new FormData(e.target);
+    const User = formData.get("User");
+    const Password = formData.get("Password");
+
+    console.log(User, Password);
+
+    login(User, Password);
     navigate("/home");
   }
 
   return (
-    <form onSubmit={handleSubmit} className="justify-end flex p-4 text-center bg-primary space-x-2">
+    <form
+      onSubmit={handleSubmit}
+      className="flex justify-end space-x-2 bg-primary p-4 text-center"
+    >
       <h2 className="">welcome</h2>
-        <input placeholder="User" className=""/>
-        <input placeholder="Password" type="password" className="" />
-        <button type="submit" className="bg-success">Login</button>
+      <input name="User" placeholder="User" className="" />
+      <input
+        name="Password"
+        placeholder="Password"
+        type="password"
+        className=""
+      />
+      <button type="submit" className="bg-success">
+        Login
+      </button>
     </form>
   );
 }
